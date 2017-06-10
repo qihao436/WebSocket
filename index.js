82176046f4d2c211b17f49181823a46c8d8f7dbb
qihao436 <qihao436@hotmail.com>
2017-06-10 15:51:48 -0400

    test

---
+++ index.js
@@ -0,0 +1,17 @@
+var net = require('net');
+
+var port =12345;
+
+net.createServer(function(socket){
+  console.log('Connected: ' + socket.remoteAddress + ' : ' + socket.remotePort);
+
+  socket.on('data',function(data){
+    console.log('DATA' + socket.remoteAddress + ' : ' + data);
+  });
+
+  socket.on('close',function(data){
+      console.log('CLOSED' + socket.remoteAddress + ' : ' + data);
+  });
+}).listen(port);
+
+console.log('listening on: ' +port);
